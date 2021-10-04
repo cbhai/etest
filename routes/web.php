3a42a55861520e8b8ccec21ef2f4b05b0400e233
Nitin <info@contractorbhai.com>
2021-10-04 09:35:46 +0530

    1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'deployed on cloudways';
+    return 'deployed on cloudways!';
 });
 
 Route::get('/users', function () {
